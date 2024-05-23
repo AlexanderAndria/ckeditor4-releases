4d68240b7f5ba369d12c1e3be54a91fe5e19a2c5
Andrianov <andatr2014@yandex.ru>
2024-05-23 17:41:05 +0300

    init

---
+++ config.js
@@ -0,0 +1,12 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+  config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}&&api_key=06d91989663a56b6659667'
+
+};
